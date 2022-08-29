ff0d263ba03105c69876667915a2f78b6509dbee
CJ Pais <cj@cjpais.com>
2022-08-29 12:07:33 -0600

    footer tweaks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      Crafted by <a href="https://relational.fyi">Relational</a>
+    <div className="flex flex-col justify-center pt-7 gap-1 pb-2">
+      <div className="flex gap-4">
+        <a href="https://etherscan.io/address/0xDf01A4040493B514605392620B3a0a05Eb8Cd295">
+          Contract
+        </a>
+        &bull;{" "}
+        <a href="https://www.npmjs.com/package/@exquisite-graphics/js">NPM</a>{" "}
+        &bull;{" "}
+        <a href="https://github.com/xqstgfx/exquisite-graphics">GitHub</a>
+      </div>
+      <div className="flex gap-2 justify-center">
+        Crafted by <a href="https://relational.fyi">Relational</a>
+      </div>
     </div>
   );
 };
